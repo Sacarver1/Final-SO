590fabae55725c20b116764fad2f4b890aa15a9b
Sophie <133617236+Sacarver1@users.noreply.github.com>
2023-06-06 15:37:36 -0500

    Add files via upload

--- GestorTexto.c
+++ GestorTexto.c
@@ -1,143 +1,198 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
 #include <ncurses.h>
+#include <string.h>
 
-#define MAX_LINE_LENGTH 80 // Longitud máxima de una línea
+void abrirArchivo(const char* nombreArchivo) {
+    FILE* archivo = fopen(nombreArchivo, "r");
+    if (archivo == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
 
-typedef struct {
-    char nombre[100];
-    char contenido[1000];
-} Archivo;
+    char caracter;
+    while ((caracter = fgetc(archivo)) != EOF) {
+        printf("%c", caracter);
+    }
 
-void* hiloFormatoDesborde(void* arg) {
-    Archivo* archivo = (Archivo*)arg;
+    fclose(archivo);
+}
 
-    while (1) {
-        // Obtener el ancho de la pantalla
-        struct winsize w;
-        ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-        int anchoPantalla = w.ws_col;
+void guardarArchivo(const char* nombreArchivo, const char* contenido) {
+    FILE* archivo = fopen(nombreArchivo, "w");
+    if (archivo == NULL) {
+        printf("No se pudo guardar el archivo.\n");
+        return;
+    }
 
-        // Copiar el texto original para modificarlo
-        char textoFormateado[1000];
-        strcpy(textoFormateado, archivo->contenido);
+    fprintf(archivo, "%s", contenido);
 
-        // Verificar cada línea y ajustar el formato si se desborda el ancho de la pantalla
-        char* linea = strtok(textoFormateado, "\n");
-        while (linea != NULL) {
-            if (strlen(linea) > anchoPantalla) {
-                // Ajustar formato de la línea para evitar el desborde
-                int i;
-                for (i = anchoPantalla; i < strlen(linea); i += anchoPantalla) {
-                    memmove(&linea[i + 1], &linea[i], strlen(linea) - i);
-                    linea[i] = '\n';
-                }
-            }
+    fclose(archivo);
+}
 
-            linea = strtok(NULL, "\n");
+void escribirArchivo(const char* nombreArchivo) {
+    FILE* archivo = fopen(nombreArchivo, "a");
+    if (archivo == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
+
+    printf("Ingrese el texto a escribir en el archivo (Ctrl+D para finalizar):\n");
+    char linea[100];
+    while (fgets(linea, sizeof(linea), stdin) != NULL) {
+        fputs(linea, archivo);
+    }
+
+    fclose(archivo);
+    printf("Archivo actualizado exitosamente.\n");
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <pthread.h>
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* cambiarColorHilo(void* arg) {
+    int opcion = *((int*)arg);
+
+    pthread_mutex_lock(&mutex);
+
+    if (opcion >= 1 && opcion <= 4) {
+        int colorPair = 0;
+
+        switch (opcion) {
+            case 1: // Rojo
+                colorPair = COLOR_PAIR(1);
+                break;
+
+            case 2: // Verde
+                colorPair = COLOR_PAIR(2);
+                break;
+
+            case 3: // Azul
+                colorPair = COLOR_PAIR(3);
+                break;
+
+            case 4: // Amarillo
+                colorPair = COLOR_PAIR(4);
+                break;
         }
 
-        // Mostrar el texto formateado en pantalla
-        clear();
-        printw("%s\n", textoFormateado);
-        refresh();
-
-        sleep(1); // Esperar 1 segundo antes de verificar el formato nuevamente
+        attron(colorPair);
+        printw("Color de letra cambiado.\n");
+        attroff(colorPair);
+    } else {
+        printw("Opción inválida. Intente de nuevo.\n");
     }
+
+    refresh();
+    getch();
+
+    pthread_mutex_unlock(&mutex);
 
     return NULL;
-}
-
-void abrirArchivo(Archivo* archivo) {
-    clear();
-    printw("Opción: Abrir archivo\n");
-    printw("Ingrese el nombre del archivo: ");
-    refresh();
-
-    scanw("%s", archivo->nombre);
-    // Implementación para abrir el archivo
-    // Aquí puedes leer el contenido del archivo y almacenarlo en archivo->contenido
-    printw("Archivo abierto exitosamente.\n");
-    refresh();
-    getch();
-}
-
-void guardarArchivo(Archivo* archivo) {
-    clear();
-    printw("Opción: Guardar archivo\n");
-    // Implementación para guardar el archivo actual
-    // Aquí puedes guardar el contenido de archivo->contenido en el archivo actual
-    printw("Archivo guardado exitosamente.\n");
-    refresh();
-    getch();
-}
-
-void guardarComo(Archivo* archivo) {
-    clear();
-    printw("Opción: Guardar como...\n");
-    printw("Ingrese el nombre del archivo: ");
-    refresh();
-
-    scanw("%s", archivo->nombre);
-    // Implementación para guardar el archivo con otro nombre
-    // Aquí puedes guardar el contenido de archivo->contenido en un archivo con el nuevo nombre
-    printw("Archivo guardado como '%s' exitosamente.\n", archivo->nombre);
-    refresh();
-    getch();
-}
-
-void escribirArchivo(Archivo* archivo) {
-    clear();
-    printw("Opción: Escribir archivo\n");
-    printw("Ingrese el contenido del archivo:\n");
-    refresh();
-
-    // Leer el contenido del archivo línea por línea
-    int lineas = 0;
-    char linea[MAX_LINE_LENGTH];
-    while (1) {
-        scanw("%s", linea);
-
-        if (strcmp(linea, ":q") == 0) {
-            break;
-        }
-
-        strcat(archivo->contenido, linea);
-        strcat(archivo->contenido, "\n");
-        lineas++;
-    }
-
-    printw("Archivo escrito exitosamente. (%d líneas)\n", lineas);
-    refresh();
-    getch();
 }
 
 void cambiarColorLetra() {
     clear();
     printw("Opción: Cambiar color de la letra\n");
-    // Implementación para cambiar el color de la letra
-    printw("Color de letra cambiado exitosamente.\n");
+    printw("Seleccione el color de la letra:\n");
+    printw("1. Rojo\n");
+    printw("2. Verde\n");
+    printw("3. Azul\n");
+    printw("4. Amarillo\n");
+    printw("Ingrese su elección: ");
+
+    int opcion;
+    scanw("%d", &opcion);
+
+    pthread_t hilo;
+    pthread_create(&hilo, NULL, cambiarColorHilo, (void*)&opcion);
+    pthread_join(hilo, NULL);
+}
+
+
+void configuracionAutoguardado(int* tiempoAutoguardado) {
+    printf("Configuración de Autoguardado\n");
+    printf("Ingrese el tiempo de autoguardado (en segundos): ");
+    scanf("%d", tiempoAutoguardado);
+    printf("La configuración de autoguardado se ha guardado.\n");
+}
+
+void configuracionEsquemaColores(int* esquemaColores) {
+    printf("Configuración de Esquema de Colores\n");
+    printf("Seleccione el esquema de colores:\n");
+    printf("1. Claro\n");
+    printf("2. Oscuro\n");
+    printf("Ingrese su elección: ");
+    scanf("%d", esquemaColores);
+    printf("La configuración de esquema de colores se ha guardado.\n");
+}
+
+void abrirArchivoInterfaz(const char* nombreArchivo) {
+    clear();
+    printw("Opción: Abrir archivo\n");
+    abrirArchivo(nombreArchivo);
     refresh();
     getch();
 }
 
-void configuracion() {
+void guardarArchivoInterfaz(const char* nombreArchivo, const char* contenido) {
     clear();
-    printw("Opción: Configuración\n");
-    // Implementación para la configuración
-    printw("Configuración realizada exitosamente.\n");
+    printw("Opción: Guardar archivo\n");
+    guardarArchivo(nombreArchivo, contenido);
     refresh();
     getch();
+}
+
+void escribirArchivoInterfaz(const char* nombreArchivo) {
+    clear();
+    printw("Opción: Escribir archivo\n");
+    escribirArchivo(nombreArchivo);
+    refresh();
+    getch();
+}
+
+void cambiarColorLetraInterfaz() {
+    clear();
+    printw("Opción: Cambiar color de la letra\n");
+    cambiarColorLetra();
+    refresh();
+    getch();
+}
+
+void configuracionInterfaz(int* tiempoAutoguardado, int* esquemaColores) {
+    clear();
+    printw("Opción: Configuración\n");
+    printw("1. Configurar Autoguardado\n");
+    printw("2. Configurar Esquema de Colores\n");
+    printw("Seleccione una opción de configuración: ");
+    refresh();
+
+    char opcion;
+    scanf(" %c", &opcion);
+
+    switch (opcion) {
+        case '1':
+            configuracionAutoguardado(tiempoAutoguardado);
+            break;
+        case '2':
+            configuracionEsquemaColores(esquemaColores);
+            break;
+        default:
+            printw("Opción inválida. Intente de nuevo.\n");
+            refresh();
+            getch();
+            break;
+    }
 }
 
 int main() {
-    pthread_t hilo;
-    Archivo archivo;
     char opcion;
+    const char* nombreArchivo = "archivo.txt";
+    const char* contenidoArchivo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
 
     initscr(); // Inicializar la pantalla de ncurses
     keypad(stdscr, TRUE); // Habilitar el uso de teclas especiales
@@ -149,11 +204,10 @@ int main() {
         printw("\nOpciones:\n");
         printw("1. Abrir archivo\n");
         printw("2. Guardar archivo\n");
-        printw("3. Guardar como...\n");
-        printw("4. Escribir archivo\n");
-        printw("5. Cambiar color de la letra\n");
-        printw("6. Configuración\n");
-        printw("7. Salir\n");
+        printw("3. Escribir archivo\n");
+        printw("4. Cambiar color de la letra\n");
+        printw("5. Configuración\n");
+        printw("6. Salir\n");
         printw("Seleccione una opción: ");
         refresh();
 
@@ -161,35 +215,31 @@ int main() {
 
         switch (opcion) {
             case '1': // Abrir archivo
-                abrirArchivo(&archivo);
+                abrirArchivoInterfaz(nombreArchivo);
                 break;
 
             case '2': // Guardar archivo
-                guardarArchivo(&archivo);
+                guardarArchivoInterfaz(nombreArchivo, contenidoArchivo);
                 break;
 
-            case '3': // Guardar como...
-                guardarComo(&archivo);
+            case '3': // Escribir archivo
+                escribirArchivoInterfaz(nombreArchivo);
                 break;
 
-            case '4': // Escribir archivo
-                escribirArchivo(&archivo);
+            case '4': // Cambiar color de la letra
+                cambiarColorLetraInterfaz();
                 break;
 
-            case '5': // Cambiar color de la letra
-                cambiarColorLetra();
+            case '5': // Configuración
+                configuracionInterfaz(&tiempoAutoguardado, &esquemaColores);
                 break;
 
-            case '6': // Configuración
-                configuracion();
-                break;
-
-            case '7': // Salir
+            case '6': // Salir
                 clear();
                 printw("Saliendo del programa...\n");
                 refresh();
                 endwin(); // Finalizar la pantalla de ncurses
-                exit(0);
+                return 0;
 
             default:
                 clear();
@@ -199,14 +249,6 @@ int main() {
                 break;
         }
     }
-
-    // Crear el hilo de formato de desborde
-    if (pthread_create(&hilo, NULL, hiloFormatoDesborde, (void*)&archivo) != 0) {
-        printw("Error al crear el hilo de formato de desborde.\n");
-        exit(1);
-    }
-
-    pthread_join(hilo, NULL); // Esperar a que el hilo termine (esto no se ejecutará en este ejemplo)
 
     return 0;
 }
